fae44580759158b6273299f42ba6313c571d7cc7
Leonardo (Notebook) <lnrd.ferreira@hotmail.com>
2019-03-23 21:22:09 -0300

    Add player movement

---
+++ Command Pattern/Assets/Scripts/Command.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Command{
+
+	public abstract void Execute(GameObject gameObject); 
+
+}
+
+public class MoveFoward : Command 
+{
+	public override void Execute(GameObject gameObject)
+	{
+		gameObject.transform.Translate(gameObject.transform.forward);
+	}
+}
---
+++ Command Pattern/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 1f;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(transform.forward * speed);
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(transform.forward * -speed);
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(transform.right * -speed);
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(transform.right * speed);
+		}
+		if (Input.GetKey(KeyCode.Space))
+		{
+			Shoot();
+		}
+	}
+
+	void Shoot()
+	{
+	}
+}
